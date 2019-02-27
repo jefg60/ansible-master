410417c52e6e74f35ebae947baa4ec4ab4c9eba0
Jeff Hibberd <jeff@jeffhibberd.com>
2019-02-27 19:29:17 +0000

    fix vagrant tests

--- vagrant-test-env/ansible-ping.sh
+++ vagrant-test-env/ansible-ping.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+export SSH_ASKPASS=/usr/bin/ssh-askpass
+export DISPLAY=:0
+eval $(ssh-agent -s)
+nohup ssh-add
 /opt/ansible-master/bin/ansible all -i /vagrant/inventory-internal -m ping
