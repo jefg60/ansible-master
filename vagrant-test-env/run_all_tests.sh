3e7c7e70e6bd04cf351962665875f9c2567d9449
Jeff Hibberd <jeff@jeffhibberd.com>
2019-11-20 17:44:08 +0000

    add a sleep before bats tests to ensure no errors

--- vagrant-test-env/run_all_tests.sh
+++ vagrant-test-env/run_all_tests.sh
@@ -2,7 +2,7 @@
 
 source /opt/ansible-master/bin/activate
 
-sleep 120
+sleep 120 #ensure time has passed to allow tests to work
 bats /vagrant/tests/ | tee /vagrant/tests/results/TAP
 
 #This causes us to exit with the exit code of the bats not the tee
